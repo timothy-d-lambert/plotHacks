2acc92ebf81c863a12e8a8174aa63aa1d0459290
evanchildress <evan.childress@gmail.com>
2016-02-05 10:23:09 -0500

    adding package files

---
+++ R/error.bar.r
@@ -0,0 +1,64 @@
+#' Plots error bars on a base R plot
+#'@param x a numeric vector palcement of error bars on the x-axis
+#'@param y a numeric vector of the y-origins of the error bars
+#'@param upper.y A vector of the upper y limits for error bars
+#'@param lower.y A vector of the lower y limits for error bars, set to upper.y by default
+#'@param upper.x A vector of the upper x limits for error bars
+#'@param lower.x A vector of the lower x limits for error bars, set to upper.x by default
+#'@param length the length of the cap on the error bars
+#'@param x.bar Logical indicating whether to include horizontal error bars
+#'@param y.bar Logical indicating whether to include vertical error bars
+#'@param interval.type If this is set to "dist", upper.y is set to y+upper.y
+#'@details Puts error bars on a plot. The default has only y error bars, you must define the vectors x,y, and upper.y (your error vector) at a minimum. Lower and upper error can be set separately by also defining lower.y. To display x error bars set x.bar=T and upper.x to a vector containing your x error. type is set to either "dist" as in distance from the mean or "value" which is the actual lower/upper bound
+#'@export
+
+error.bar <- function(x, y, upper.y,
+                      lower.y=upper.y,upper.x,lower.x=upper.x,
+                      length=0.03,x.bar=F,y.bar=T,interval.type="dist",...){
+if(length(x) != length(y) | length(y) !=length(lower.y) |
+     length(lower.y) != length(upper.y))
+{stop("vectors must be same length")}
+
+
+
+if(y.bar==T){
+  if(interval.type=="dist"){
+  upper.y<-y+upper.y
+  lower.y<-y-lower.y
+}
+
+  arrows(x,upper.y,x,lower.y, angle=90,
+         code=3, length=length, ...)}
+if(x.bar==T){
+  if(interval.type=="dist"){
+  upper.x<-x+upper.x
+  lower.x<-x-lower.x
+}
+  arrows(upper.x,y,lower.x,y,angle=90,code=3,length=length,...)
+}
+}
+
+
+#' Plots error as a polygon on a base R plot, maybe, I haven't really tested or used this much
+#'@param x a numeric vector palcement of error bars on the x-axis
+#'@param y a numeric vector of the y-origins of the error bars
+#'@param upper.y A vector of the upper y limits for error bars
+#'@param lower.y A vector of the lower y limits for error bars, set to upper.y by default
+#'@param col Color of the polygon
+#'@param border Color for the border of the polygon
+#'@param interval.type If this is set to "dist", upper.y is set to y+upper.y
+#'@details Puts error bars on a plot. The default has only y error bars, you must define the vectors x,y, and upper.y (your error vector) at a minimum. Lower and upper error can be set separately by also defining lower.y. To display x error bars set x.bar=T and upper.x to a vector containing your x error. type is set to either "dist" as in distance from the mean or "value" which is the actual lower/upper bound
+#'@export
+
+error.band<-function(x, y, upper.y,
+  lower.y=upper.y,col='gray',border=NA,interval.type="dist",...){
+  if(length(x) != length(y) | length(y) !=length(lower.y) |
+     length(lower.y) != length(upper.y))
+{stop("vectors must be same length")}
+if(interval.type=="dist"){
+  upper.y<-y+upper.y
+  lower.y<-y-lower.y
+}
+  polygon(c(x,rev(x)),
+      c(upper.y,rev(lower.y)),col=col,border=border)
+}
---
+++ R/mutliHist.R
@@ -0,0 +1,44 @@
+#' Creates a stacked histogram (as opposed to side-by-side or overlapping) for categorical data
+#'@param data is a data.frame or data.table containing the data
+#'@param y is the variable to be plotted as a histogram as a quoted name
+#'@param group is the quoted name of the grouping variable
+#'@param breaks is a vector of breaks for the histogram
+#'@param col is an optional specification of the colors to be used in order of unique(group)
+#'@param axisStep is the spacing of x-axis labels
+#'@return A stacked histogram color coded by the provided factor
+#'
+#'@export
+
+multiHist<-function(data,y,group,breaks,col=NULL,axisStep=1,xlimit=NULL,logAxis=F,axisLim=NULL,...){
+  groups<-unique(data[[group]])
+  countNames<-list()
+  for(g in 1:length(groups)){
+    assign(paste0("hist",g),
+           hist(data[[y]][which(data[[group]]==groups[g])],
+                breaks=breaks,plot=F))
+    assign(paste0("counts",g),get(paste0("hist",g))$count)
+    countNames[[g]]<-as.name(paste0("counts",g))
+  }
+  if(is.null(col)){
+    col<-palette()[1:length(groups)]
+  }
+
+  counts<-do.call(cbind,args=countNames,quote=F)
+  if(is.null(xlimit)){
+    barplot(t(counts),col=col,border=NA,space=0,...)
+  } else {
+    xlimit<-c(which.min(abs(xlimit[1]-hist1$breaks)),which.min(abs(xlimit[2]-hist1$breaks)))
+    barplot(t(counts),col=col,border=NA,space=0,xlim=xlimit,...)}
+
+  if(is.null(axisLim)){
+    axisSeq<-seq(min(breaks),max(breaks),axisStep)
+  } else {axisSeq<-seq(axisLim[1],axisLim[2],axisStep)}
+
+  axisLabels<-axisSeq
+  a<-lm(I(1:length(breaks))~breaks)
+  axisAt<-predict(a,data.frame(breaks=axisSeq))-1
+  if(logAxis){axisLabels<-10^axisLabels}
+  axis(1,axisAt,axisLabels)
+  box(bty='l')
+}
+
